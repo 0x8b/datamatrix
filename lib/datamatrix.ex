6cdd9cab4501b3b9fad9b126142f8a0ab219668c
0x8b <temporary.gumienny@gmail.com>
2020-01-22 13:15:29 +0100

    add DataMatrix module

--- lib/datamatrix.ex
+++ lib/datamatrix.ex
@@ -2,4 +2,40 @@ defmodule DataMatrix do
   @moduledoc """
   Documentation for DataMatrix.
   """
+
+  alias DataMatrix.{Bits, Encode, Matrix, ReedSolomon, SymbolAttribute, SVG}
+
+  def encode(data, quiet_zone \\ 1) when is_binary(data) do
+    {version, encoded} = Encode.encode(data)
+
+    blocks = SymbolAttribute.interleaved_blocks(version)
+    errors = SymbolAttribute.total_error_codewords(version)
+
+    ecc =
+      0..(blocks - 1)
+      |> Stream.map(fn offset ->
+        dropped =
+          Enum.drop(:binary.bin_to_list(encoded), offset)
+          |> Enum.take_every(blocks)
+          |> :binary.list_to_bin()
+
+        ReedSolomon.encode(dropped, div(errors, blocks))
+      end)
+      |> Enum.map(&:binary.bin_to_list/1)
+      |> Enum.zip()
+      |> Enum.flat_map(&Tuple.to_list(&1))
+      |> :binary.list_to_bin()
+
+    bits = (encoded <> ecc) |> Bits.extract()
+
+    Matrix.new(version)
+    |> Matrix.draw_patterns()
+    |> Matrix.draw_data(bits)
+    |> Matrix.draw_quiet_zone(quiet_zone)
+    |> Matrix.export()
+  end
+
+  def format(matrix, :svg, options \\ []) do
+    SVG.format(matrix, options)
+  end
 end
--- test/datamatrix_test.exs
+++ test/datamatrix_test.exs
@@ -1,4 +1,109 @@
 defmodule DataMatrixTest do
   use ExUnit.Case
   doctest DataMatrix
+
+  test "30Q324343430794<OQQ" do
+    data_matrix = """
+    █░█░█░█░█░█░█░█░
+    █░█░█░█░█░░░░░░█
+    █░█░█░█░███░██░░
+    █░█░█░█░█░██░░██
+    █░█░█░█░█░░░██░░
+    █░█░█░█░█░░░██░█
+    █░█░█░█░█░░░░█░░
+    █░█░█░█░░██░█░░█
+    █░█░█░█░█░░░░██░
+    █░░░░░█░░█░██░░█
+    █████████░░░░█░░
+    ██░██░░██░░█░█░█
+    ███████░░██░░█░░
+    ███░░█░████░░█░█
+    ███░░█░░█░█░░░█░
+    ████████████████
+    """
+
+    expected = %{
+      version: 3,
+      nrow: 16,
+      ncol: 16,
+      matrix: to_matrix(data_matrix, 16)
+    }
+
+    assert DataMatrix.encode("30Q324343430794<OQQ", 0) == expected
+  end
+
+  test "123456" do
+    data_matrix = """
+    ░░░░░░░░░░░░░░
+    ░░░░░░░░░░░░░░
+    ░░█░█░█░█░█░░░
+    ░░██░░█░██░█░░
+    ░░██░░░░░█░░░░
+    ░░██░░░███░█░░
+    ░░██░░░░█░░░░░
+    ░░█░░░░░████░░
+    ░░███░██░░░░░░
+    ░░████░██░░█░░
+    ░░█░░███░█░░░░
+    ░░██████████░░
+    ░░░░░░░░░░░░░░
+    ░░░░░░░░░░░░░░
+    """
+
+    expected = %{
+      version: 0,
+      nrow: 14,
+      ncol: 14,
+      matrix: to_matrix(data_matrix, 14)
+    }
+
+    assert DataMatrix.encode("123456", 2) == expected
+  end
+
+  @tag :skip
+  test "A1B2C3D4E5F6G7H8I9J0K1L2" do
+    data_matrix = """
+    █░█░█░█░█░█░█░█░█░
+    █░█░░░█░█░█░░░████
+    █░██░░░░░███░░░░█░
+    █░░░░░█░███░█░░███
+    █░░█░░░░░█░░░███░░
+    █░█░█████░█░█░████
+    █░░██░░███████░██░
+    ██░░███░████████░█
+    ██████████░█████░░
+    █░███░██░█░░█░██░█
+    █░░░█░██░█░█░░███░
+    █░██░████░░██░░░░█
+    ██░░░██░█░█░░████░
+    ██░██░█░░░░░█░░░██
+    █░██░█░░██░█░██░█░
+    █░░░█░░██░░█░██░██
+    █░░░██░░░░░░█░░█░░
+    ██████████████████
+    """
+
+    expected = %{
+      version: 0,
+      nrow: 18,
+      ncol: 18,
+      matrix: to_matrix(data_matrix, 18)
+    }
+
+    assert DataMatrix.encode("A1B2C3D4E5F6G7H8I9J0K1L2", 0) == expected
+  end
+
+  defp to_matrix(datamatrix, ncol) do
+    datamatrix
+    |> String.trim()
+    |> String.replace("\n", "")
+    |> String.graphemes()
+    |> Stream.map(fn module ->
+      case module do
+        "█" -> 1
+        "░" -> 0
+      end
+    end)
+    |> Enum.chunk_every(ncol)
+  end
 end
