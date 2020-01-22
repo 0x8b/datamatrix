5b73374e63a95d254ed766a2997272cffee967d6
0x8b <temporary.gumienny@gmail.com>
2020-01-22 13:14:27 +0100

    add symbol attributes

--- lib/datamatrix/symbol_attribute.ex
+++ lib/datamatrix/symbol_attribute.ex
@@ -44,6 +44,105 @@ defmodule DataMatrix.SymbolAttribute do
 
   Module.put_attribute(__MODULE__, :symbol_size, List.to_tuple(symbol_size))
 
+  @total_data_codewords {
+    3,
+    5,
+    8,
+    12,
+    18,
+    22,
+    30,
+    36,
+    44,
+    62,
+    86,
+    114,
+    144,
+    174,
+    204,
+    280,
+    368,
+    456,
+    576,
+    696,
+    816,
+    1050,
+    1304,
+    1558,
+    5,
+    10,
+    16,
+    22,
+    32,
+    49
+  }
+
+  @total_error_codewords {
+    5,
+    7,
+    10,
+    12,
+    14,
+    18,
+    20,
+    24,
+    28,
+    36,
+    42,
+    48,
+    56,
+    68,
+    84,
+    112,
+    144,
+    192,
+    224,
+    272,
+    336,
+    408,
+    496,
+    620,
+    7,
+    11,
+    14,
+    18,
+    24,
+    28
+  }
+
+  @interleaved_blocks {
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    2,
+    2,
+    4,
+    4,
+    4,
+    4,
+    6,
+    6,
+    8,
+    10,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
+  }
+
   def size(version) do
     elem(@symbol_size, version)
   end
@@ -74,5 +173,21 @@ defmodule DataMatrix.SymbolAttribute do
       16 -> {4, 4}
       36 -> {6, 6}
     end
+  end
+
+  def total_data_codewords do
+    Tuple.to_list(@total_data_codewords)
+  end
+
+  def data_capacity(version) do
+    elem(@total_data_codewords, version)
+  end
+
+  def total_error_codewords(version) do
+    elem(@total_error_codewords, version)
+  end
+
+  def interleaved_blocks(version) do
+    elem(@interleaved_blocks, version)
   end
 end
