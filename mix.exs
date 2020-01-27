a01faaeabb4cb3d164f333eaee53569a4a91bdb1
0x8b <temporary.gumienny@gmail.com>
2020-01-27 10:45:56 +0100

    bump 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DataMatrix.MixProject do
   def project do
     [
       app: :data_matrix,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
